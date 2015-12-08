4226c14a6964d90e7f34a4408924b97ee38e8e0a
Rodrigo Moyle <rodrigorm@gmail.com>
2015-12-08 21:00:40 -0200

    Removed code from starter kit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers }    from 'redux';
 import { routeReducer }       from 'redux-simple-router';
-import counter                from './counter';
 import user                   from './user';
 import repo                   from './repo';
 import issue                  from './issue';
 
 export default combineReducers({
-  counter,
   user,
   repo,
   issue,
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,6 @@
 import React                 from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, Redirect } from 'react-router';
 import CoreLayout            from 'layouts/CoreLayout';
-import HomeView              from 'views/HomeView';
-import AboutView             from 'views/AboutView';
 import UserView              from 'views/UserView';
 import UserIndexView         from 'views/user/IndexView';
 import RepoView              from 'views/user/RepoView';
@@ -11,8 +9,6 @@ import IssueView             from 'views/user/repo/IssueView';
 
 export default (
   <Route            component={CoreLayout}   path='/'>
-    <IndexRoute     component={HomeView} />
-    <Route          component={AboutView}    path='/about' />
     <Route          component={UserView}     path='/:username'>
       <IndexRoute   component={UserIndexView} />
       <Route        component={RepoView} path=':repo_name'>
